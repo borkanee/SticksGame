8b9fbc20cff495439b33b10078d51eb44a2024e8
nikibrodin <nikibrodin@gmail.com>
2018-11-16 15:59:44 +0100

    RED

--- tests/SticksTest.cs
+++ tests/SticksTest.cs
@@ -6,9 +6,20 @@ namespace SticksGame
     public class SticksTest
     {
 
+
         [Fact]
         public void sticksShouldBeInstantiatedWithoutParameters() {
             Sticks sut = new Sticks();
+        }
+
+        [Fact]
+        public void SticksShouldHaveTwentyOneSticksFromStart()
+        {
+
+            Sticks sut = new Sticks();
+            int actual = sut.NumberOfSticks;
+            int expected = 21;
+            Assert.Equal(expected, actual);
         }
     }
 }
