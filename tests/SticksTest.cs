6530bd543a96fbe14e252bc92d3c5bcda3e280f8
nikibrodin <nikibrodin@gmail.com>
2018-11-16 16:09:47 +0100

    REFACTOR

--- tests/SticksTest.cs
+++ tests/SticksTest.cs
@@ -5,17 +5,11 @@ namespace SticksGame
 {
     public class SticksTest
     {
-
-
-        [Fact]
-        public void sticksShouldBeInstantiatedWithoutParameters() {
-            Sticks sut = new Sticks();
-        }
+        Sticks sut = new Sticks();
 
         [Fact]
         public void SticksShouldHaveTwentyOneSticksFromStart()
         {
-            Sticks sut = new Sticks();
             int actual = sut.NumberOfSticks;
             int expected = 21;
             Assert.Equal(expected, actual);
