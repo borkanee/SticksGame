731eac5b237fe49301f9e73bc6752118bdb47866
borkanee <grubesic.boris@gmail.com>
2018-11-21 12:07:39 +0100

    GREEN

---
+++ SticksGame/Game.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SticksGame
+{
+    public class Game
+    {
+        private View _view;
+        private Sticks _sticks;
+        private AIPlayer _AIPlayer;
+
+        public Game(View view, Sticks sticks, AIPlayer AIPlayer)
+        {
+            _view = view;
+            _sticks = sticks;
+            _AIPlayer = AIPlayer;
+        }
+    }
+}
